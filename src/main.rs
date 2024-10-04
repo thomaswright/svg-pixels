5642e5c57719dd1e393fac8d2764dcdd59d47c7b
Thomas Wright <thomaswright1@gmail.com>
2024-10-04 14:09:12 +0900

    add crips edges to remove antialiasing

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() -> io::Result<()> {
 
     writeln!(
         file,
-        r#"<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 {} {}">"#,
+        r#"<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 {} {}" shape-rendering="crispEdges">"#,
         width, height
     )?;
 
